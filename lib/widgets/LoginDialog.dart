0962bb18f8b6a2248fd2f29598d39d2969838424
ariandi <dussewi@gmail.com>
2020-11-13 01:32:59 -0500

    mejorando el login

--- lib/widgets/LoginDialog.dart
+++ lib/widgets/LoginDialog.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:ds_richmeat_form/screens/FormMenuScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginDialog extends StatefulWidget {
   @override
@@ -13,6 +13,7 @@ class _LoginDialogState extends State<LoginDialog> {
   final TextEditingController userController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   static const _url = 'https://rm-form-backend.herokuapp.com/richmeat/login';
+  bool _mantener_autenticado = true;
 
   void irAlMenu(BuildContext context) async {
     Navigator.pop(context);
@@ -20,86 +21,120 @@ class _LoginDialogState extends State<LoginDialog> {
   }
 
   Future<bool> login(BuildContext context, String usuario, String clave) async {
-    HttpClient httpClient = new HttpClient();
-    HttpClientRequest request = await httpClient.postUrl(Uri.parse(_url));
-    request.headers.set('content-type', 'application/json');
-    request.add(
-        utf8.encode(json.encode({'userName': usuario, 'password': clave})));
-    HttpClientResponse response = await request.close();
-    // todo - you should check the response.statusCode
-    String reply = await response.transform(utf8.decoder).join();
-    httpClient.close();
-    print('resp= $reply');
+    http
+        .post(
+      _url,
+      headers: {'content-type': 'application/json'},
+      body: json.encode(
+        {'userName': usuario, 'password': clave},
+      ),
+    )
+        .then((response) {
+      print('status::${response.statusCode}');
+      if (response.statusCode == 201) {
+        if (response.body == 'login:true') {
+          irAlMenu(context);
+        } else {
+          showMyDialog(
+              context, 'Error', 'Usuario o contraseña incorrecta.', 'Aceptar');
+        }
+      } else {
+        showMyDialog(
+            context, 'Error', 'Hay un problema en la comunicación.', 'Aceptar');
+      }
+    }).catchError((err) {
+      showMyDialog(
+          context,
+          'Error',
+          'Ha ocurrido un error de red.'
+              '\nAsegúrese de estar conectado a internet.',
+          'Aceptar');
+    });
+  }
 
-    if (reply == 'login:true') {
-      irAlMenu(context);
-    } else {
-      showDialog(
-          context: context,
-          builder: (ctx) {
-            return AlertDialog(
-              title: Text('Error'),
-              content: Text('Usuario o contraseña incorrecta.'),
-              actions: [
-                FlatButton(
-                  child: Text('ACEPTAR'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                )
-              ],
-            );
-          });
-    }
+  void showMyDialog(
+      BuildContext context, String titulo, String mensaje, String accion) {
+    showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text(titulo),
+            content: Text(mensaje),
+            actions: [
+              FlatButton(
+                child: Text(accion),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("xddx"),
+        title: Text("Entrar"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          child: ListView(
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: "Usuario"),
-                controller: userController,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: "Contraseña"),
-                controller: passwordController,
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Center(
+        child: Container(
+          height: 400,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              child: ListView(
                 children: [
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: true,
-                        onChanged: (checked) {},
-                      ),
-                      Text("Mantenerme Autenticado."),
-                    ],
+                  TextField(
+                    decoration: InputDecoration(labelText: "Usuario"),
+                    controller: userController,
                   ),
-                  RaisedButton(
-                    onPressed: () {
-                      login(context, userController.text,
-                          passwordController.text);
-                    },
-                    child: Text("Entrar"),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
+                  TextField(
+                    decoration: InputDecoration(labelText: "Contraseña"),
+                    controller: passwordController,
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: _mantener_autenticado,
+                            onChanged: (checked) {
+                              setState(() {
+                                _mantener_autenticado = checked;
+                              });
+                            },
+                          ),
+                          GestureDetector(
+                            child: Text("Mantenerme Autenticado."),
+                            onTap: () {
+                              setState(() {
+                                _mantener_autenticado = !_mantener_autenticado;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          login(context, userController.text,
+                              passwordController.text);
+                        },
+                        child: Text("Entrar"),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
